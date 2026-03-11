e6b960c4b1acb873961239f21fc6811334d8299e
tin0206 <ngtrungtin02062005@gmail.com>
2026-03-11 13:05:29 +0700

    Remove device preview when build apk

--- my_pda/lib/main.dart
+++ my_pda/lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart'; // Thêm thư viện này để dùng kReleaseMode
 import 'login_screen.dart';
 
 void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
+  runApp(
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
+
+      debugShowCheckedModeBanner: false,
       title: 'PDA App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
